7d0d1727d26b0a0b92e5c5ed475c56b5663e244d
michael corbridge <mcorbridge@gmail.com>
2015-12-03 14:59:43 -0500

    add 'projects' content

--- src/js/modules/ProjectsTopic.js
+++ src/js/modules/ProjectsTopic.js
@@ -8,5 +8,21 @@ angular.module('ProjectsTopic', [])
 
 		console.log('---> projectsTopicCtrl');
 
+		$scope.goProject = function (state) {
+			var a = document.createElement('a');
+			switch (state) {
+				case 'passwordProtector':
+					a.href = 'http://mcorbridge.com/test/test.html';
+					break;
+				case 'sudoku':
+					a.href = 'http://mcorbridge.com/sudoku/html/index.html';
+					break;
+				case 'robotArm':
+					a.href = 'http://mcorbridge.com/robotArm/index.html';
+					break;
+			}
+			a.click();
+		}
+
 	}])
 
